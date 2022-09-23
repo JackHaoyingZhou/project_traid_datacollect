1fb44f866fc7f5f775077f877bf2d2e9d6829367
Ma Xihan <31639301+MXHsj@users.noreply.github.com>
2022-09-23 12:45:44 -0400

    add dist measure & data record

--- calib_probe.py
+++ calib_probe.py
@@ -1,26 +1,29 @@
 #! /usr/bin/env python3
 '''
 file name:    calib_probe.py
-description:  ECM-probe pose estimation using aruco markers
+description:  ECM-probe pose estimation using aruco markers, modified from franka_ultrasound repo
 author:       Xihan Ma
 date:         2022-09-20
 '''
 import os
 import csv
-import time
 import rospy
 import numpy as np
 from cv2 import cv2
 from cv2 import aruco
+from datetime import datetime
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+
+camera_matrix = np.array([[1811.1, 0.0, 813.3], [0.0, 1815.3, 781.8], [0.0, 0.0, 1.0]])  # ECM cam2
+dist_coeff = np.array([-0.3494, 0.4607, 0.0, 0.0])  # ECM cam2
 
 
 class StreamECMData():
   IMG_RAW_HEIGHT = 1080   # original size
   IMG_RAW_WIDTH = 1920
-  IMG_DISP_HEIGHT = 640
-  IMG_DISP_WIDTH = 480
+  IMG_DISP_HEIGHT = 480
+  IMG_DISP_WIDTH = 640
 
   def __init__(self) -> None:
     rospy.init_node('ECM_stream_subscriber', anonymous=True)
@@ -29,14 +32,17 @@ class StreamECMData():
     self.cam1_img = np.zeros((self.IMG_DISP_HEIGHT, self.IMG_DISP_WIDTH, 3), dtype=np.uint8)
     self.cam2_img = np.zeros((self.IMG_DISP_HEIGHT, self.IMG_DISP_WIDTH, 3), dtype=np.uint8)
 
+  def frm_preproc(self):
+    ...
+
   def ecm_cam1_cb(self, msg: Image) -> None:
     cam1_img_raw = CvBridge().imgmsg_to_cv2(msg).astype(np.uint8)
-    self.cam1_img = cv2.resize(cam1_img_raw, (self.IMG_DISP_HEIGHT, self.IMG_DISP_WIDTH),
+    self.cam1_img = cv2.resize(cam1_img_raw, (self.IMG_DISP_WIDTH, self.IMG_DISP_HEIGHT),
                                interpolation=cv2.INTER_AREA)
 
   def ecm_cam2_cb(self, msg: Image) -> None:
     cam2_img_raw = CvBridge().imgmsg_to_cv2(msg).astype(np.uint8)
-    self.cam2_img = cv2.resize(cam2_img_raw, (self.IMG_DISP_HEIGHT, self.IMG_DISP_WIDTH),
+    self.cam2_img = cv2.resize(cam2_img_raw, (self.IMG_DISP_WIDTH, self.IMG_DISP_HEIGHT),
                                interpolation=cv2.INTER_AREA)
 
   def get_cam1_img(self) -> np.ndarray:
@@ -48,88 +54,115 @@ class StreamECMData():
 
 class TrackMarker:
   __detect_param = aruco.DetectorParameters_create()
-  # save_path = os.path.join(os.path.dirname(__file__), 'data/marker_pos_log.csv')
+  pix_save_path = os.path.join(os.path.dirname(__file__), 'data/{}_marker_pix_log.csv'.format(datetime.now()))
+  pos_save_path = os.path.join(os.path.dirname(__file__), 'data/{}_marker_pos_log.csv'.format(datetime.now()))
 
-  def __init__(self, num_mk=1, dict_id=4):
-    if dict_id == 6:
-      self.__aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
-    elif dict_id == 4:
-      self.__aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
+  def __init__(self, num_mk=1, recording=False):
+    '''
+    tested for less than two markers with ids being 0 and 1
+    '''
+    self.__aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
     self.num_markers = num_mk
     self.mk_bbox_frame = None
     self.mk_axis_frame = None
     self.corners = None
     self.ids = None
-    self.mk_pos = np.zeros((self.num_markers, 2), dtype=int)
-    self.mk_rmat = None
-    self.mk_tvec = None
-    # self.file_out = open(self.save_path, 'a')
-    # self.writer = csv.writer(self.file_out)
+    self.mk_pix = -1*np.ones((self.num_markers, 2), dtype=np.int32)
+    self.mk_rmat = -1*np.ones((self.num_markers, 9), dtype=np.float32)
+    self.mk_tvec = -1*np.ones((self.num_markers, 3), dtype=np.float32)
+    # ===== save to file =====
+    self.doRec = recording
+    if self.doRec:
+      self.pix_file_out = open(self.pix_save_path, 'w')
+      self.pos_file_out = open(self.pos_save_path, 'w')
+      self.pix_writer = csv.writer(self.pix_file_out)
+      self.pos_writer = csv.writer(self.pos_file_out)
+
+  def __del__(self):
+    if self.doRec:
+      self.pix_file_out.close()
+      self.pos_file_out.close()
 
   def detect_markers(self, frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     self.corners, self.ids, _ = aruco.detectMarkers(
         gray, self.__aruco_dict, parameters=self.__detect_param)
-    # print(self.ids)
     self.mk_bbox_frame = aruco.drawDetectedMarkers(frame.copy(), self.corners, self.ids)
     for id in range(self.num_markers):
       try:
         curr_mk = self.corners[np.where(self.ids == id)[0][0]][0]
-        self.mk_pos[id, :] = [round(curr_mk[:, 0].mean()), round(curr_mk[:, 1].mean())]
-      except:
-        self.mk_pos[id, :] = [-1, -1]  # if marker is not detected
-      # print('id:', id+1, ' pos', self.mk_pos[id, :])
+        self.mk_pix[id, :] = [round(curr_mk[:, 0].mean()), round(curr_mk[:, 1].mean())]
+      except Exception as e:
+        # print(e)
+        self.mk_pix[id, :] = [-1, -1]  # if not detected
+    if self.doRec:
+      self.save_mk_pix()
 
-  def estimate_pose(self, frame, cam_intri, dist_coeff, mk_id=0):
-    try:
-      # print(self.corners[0])
-      # loc_marker = self.corners[np.where(self.ids == mk_id)[0][0]]
-      loc_marker = self.corners[np.where(self.ids == mk_id)[0][0]]
-      rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(loc_marker, 0.047, cam_intri, dist_coeff)
-      self.mk_axis_frame = aruco.drawAxis(frame.copy(), camera_matrix, dist_coeff, rvecs, tvecs, 0.05)
-      self.rmat = cv2.Rodrigues(rvecs)[0]       # 3x3 rotation
-      self.tvec = np.transpose(tvecs)[:, 0, 0]  # 3x1 translation
-      UV = [loc_marker[0][:, 0].mean(), loc_marker[0][:, 1].mean()]
-    except Exception as e:
-      print(e)
-      self.mk_axis_frame = frame
-      self.rmat = -1*np.ones([3, 3])
-      self.tvec = [-1, -1, -1]
-      UV = [-1, -1]
-    # print('rot: \n', self.rmat)
-    # print('trans: \n', self.tvec)
-    # return UV, rmat[:, 2], tvec
-    return UV
+  def estimate_pose(self, frame, cam_intri, dist_coeff):
+    self.mk_axis_frame = frame.copy()
+    for id in range(self.num_markers):
+      try:
+        loc_marker = self.corners[np.where(self.ids == id)[0][0]]
+        rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(loc_marker, 0.047, cam_intri, dist_coeff)
+        self.mk_axis_frame = aruco.drawAxis(self.mk_axis_frame, camera_matrix, dist_coeff, rvecs, tvecs, 0.05)
+        rmat = cv2.Rodrigues(rvecs)[0]       # 3x3 rotation
+        tvec = np.transpose(tvecs)[:, 0, 0]  # 3x1 translation
+      except Exception as e:
+        # print(e)
+        self.mk_axis_frame = frame.copy()
+        rmat = -1*np.ones([3, 3])  # if not detected
+        tvec = -1*np.ones([1, 3])
+      self.mk_rmat[id, :] = rmat.flatten()
+      self.mk_tvec[id, :] = tvec
+    if self.doRec:
+      self.save_mk_pos()
+
+  def calc_marker_dist_pix(self):
+    if self.num_markers == 2:
+      if (self.mk_pix != -1).all():
+        dist = np.sqrt((self.mk_pix[0, 0]-self.mk_pix[1, 0])**2 +
+                       (self.mk_pix[0, 1]-self.mk_pix[1, 1])**2)
+      else:
+        dist = -1
+      return dist
+    else:
+      return -1
+
+  def calc_marker_dist_xyz(self):
+    if self.num_markers == 2:
+      if (self.mk_tvec != -1).all():
+        dist = np.sqrt((self.mk_tvec[0, 0]-self.mk_tvec[1, 0])**2 +
+                       (self.mk_tvec[0, 1]-self.mk_tvec[1, 1])**2 +
+                       (self.mk_tvec[0, 2]-self.mk_tvec[1, 2])**2)
+      else:
+        dist = -1
+      return dist
+    else:
+      return -1
+
+  def save_mk_pix(self):
+    row2write = list()
+    for id in range(self.num_markers):
+      row2write.append((self.mk_pix[id, 0], self.mk_pix[id, 1]))
+    self.pix_writer.writerow(row2write)
 
   def save_mk_pos(self):
     row2write = list()
+    # append rotation matrix
     for id in range(self.num_markers):
-      row2write.append((self.mk_pos[id, 0], self.mk_pos[id, 1]))
-    self.writer.writerow(row2write)
-
-  def save_mk_pos_xyz(self, pnts):
-    row2write = list()
-    # append markers in pixels
-    for id in range(self.num_markers):
-      row2write.append((self.mk_pos[id, 0], self.mk_pos[id, 1]))
-    # append targets in xyz
-    for id in range(self.num_markers):
-      row2write.append((pnts[id, 0], pnts[id, 1], pnts[id, 2]))
-    self.writer.writerow(row2write)
-
-
-# camera_matrix = np.array([[662.1790, 0.0, 322.3619], [0.0, 662.8344, 252.0131], [0.0, 0.0, 1.0]]) # realsense
-# dist_coeff = np.array([0.0430651, -0.1456001, 0.0, 0.0])  # realsense
-camera_matrix = np.array([[1811.1, 0.0, 813.3], [0.0, 1815.3, 781.8], [0.0, 0.0, 1.0]])  # ECM cam2
-dist_coeff = np.array([-0.3494, 0.4607, 0.0, 0.0])  # ECM cam2
+      row2write.append((self.mk_rmat[id, :]))
+    # # append translation vector
+    # for id in range(self.num_markers):
+    #   row2write.append((self.mk_tvec[id, :]))
+    self.pos_writer.writerow(row2write)
 
 
 def main():
-  frm_save_path = '../data/densepose/mk_frame.png'
+  frm_save_path = 'data/mk_frame.png'
 
   ecm = StreamECMData()
-  mk_obj = TrackMarker(num_mk=2, dict_id=4)
-  rate = rospy.Rate(20)
+  mk_obj = TrackMarker(num_mk=2, recording=True)
+  rate = rospy.Rate(30)
   cv2.namedWindow('ecm2', cv2.WINDOW_AUTOSIZE)
 
   while not rospy.is_shutdown():
@@ -138,13 +171,15 @@ def main():
     mk_obj.detect_markers(frame)
     mk_obj.estimate_pose(frame, camera_matrix, dist_coeff)
 
-    tar_pix = np.zeros((mk_obj.num_markers, 2), dtype=int)
-    for i in range(mk_obj.num_markers):
-      tar_pix[i, :] = [mk_obj.mk_pos[i, 0], mk_obj.mk_pos[i, 1]]
+    dist_pix = mk_obj.calc_marker_dist_pix()
+    dist_xyz = mk_obj.calc_marker_dist_xyz()
+    # if dist_pix != -1 and dist_xyz != -1:
+    #   print(f'dist in img: {dist_pix:.3f} [pix], dist in xyz: {dist_xyz*1000:.4f} [mm]')
 
-    cv2.imshow('ecm2', mk_obj.mk_axis_frame)
+    print(f'rot: \n {mk_obj.mk_rmat.reshape((2,3,3))} \ntrans: \n {mk_obj.mk_tvec}')
+
     # cv2.imshow('ecm2', mk_obj.mk_bbox_frame)
-    print(mk_obj.mk_pos)
+    cv2.imshow('ecm2', mk_obj.mk_axis_frame)
 
     key = cv2.waitKey(1)
     if key & 0xFF == ord('q') or key == 27:
